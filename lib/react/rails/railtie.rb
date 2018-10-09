4f5f21c2a548548abedc7e2001ec941f845587cd
Phoomparin Mano <work@phoom.in.th>
2018-10-09 10:31:50 -0700

    Make react-rails transpile js files

--- lib/react/rails/railtie.rb
+++ lib/react/rails/railtie.rb
@@ -21,7 +21,7 @@ module React
 
       # Watch .jsx files for changes in dev, so we can reload the JS VMs with the new JS code.
       initializer "react_rails.add_watchable_files", group: :all do |app|
-        app.config.watchable_files.concat Dir["#{app.root}/app/assets/javascripts/**/*.jsx*"]
+        app.config.watchable_files.concat Dir["#{app.root}/app/assets/javascripts/**/*.js*"]
       end
 
       # Include the react-rails view helper lazily
@@ -91,7 +91,10 @@ module React
       initializer "react_rails.setup_engine", :group => :all do |app|
         sprockets_env = app.assets || Sprockets # Sprockets 3.x expects this in a different place
         if Gem::Version.new(Sprockets::VERSION) >= Gem::Version.new("3.0.0")
-          sprockets_env.register_mime_type("application/jsx", extensions: [".jsx", ".js.jsx", ".es.jsx", ".es6.jsx"])
+          sprockets_env.register_mime_type("application/jsx", extensions: [
+            ".js", ".jsx", ".js.jsx", ".es.jsx", ".es6.jsx",
+          ])
+
           sprockets_env.register_transformer("application/jsx", "application/javascript", React::JSX::Processor)
         else
           sprockets_env.register_engine(".jsx", React::JSX::Template)
